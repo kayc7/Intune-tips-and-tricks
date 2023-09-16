6a2e7049b1ffd4a7e473b5e0fefd8d9ebdb46ac8
kayc7 <35643272+kayc7@users.noreply.github.com>
2023-09-15 23:00:26 -0400

    Update appUninstaller.ps1

--- Force uninstall any app/appUninstaller.ps1
+++ Force uninstall any app/appUninstaller.ps1
@@ -1,11 +1,11 @@
-<#The shutdown commands featured here are specifically to prevent the uninstallers from forcing a computer reboot. Apps like Forticlient force an instant reboot after an uninstall via wmic.
-After the uninstall command is completed, it then cancels all shutdown commands in queue, including the 10 year duration and the one initiated by the uninstaller. #>
-By engaging a shutdown with a timer of 10 years, it will prevent any other reboot command from happening
+# The shutdown commands featured here are specifically to prevent the uninstallers from forcing a computer reboot. Apps like Forticlient force an instant reboot after an uninstall via wmic.
+
+## Start a timer for 10 years, preventing uninstaller shutdown command from working. /c flag with a blank string prevents a notification from popping on the user's screen. 
 shutdown /s /t 315359999 /c " " | cmd
 wmic product where "name like 'FortiClient%%'" call uninstall | cmd 
 shutdown /a | cmd 
 
-## This section is specifically to generate a file for detection to deploy this app as a Win32 app on Intune. Modify $folderPath to your liking to adjust where you want the generated file to be. Adjust the name of the generated file as needed as well. 
+### This section is specifically to generate a file for detection to deploy this app as a Win32 app on Intune. Modify $folderPath to your liking to adjust where you want the generated file to be. Adjust the name of the generated file as needed as well. 
 $folderPath = "C:\scripts"
 if (-Not (Test-Path $folderPath)) {
     New-Item -Path $folderPath -ItemType Directory
